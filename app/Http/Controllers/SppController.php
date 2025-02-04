b43a0bd6376612eb3efc4f031e954d5fdeed0be9
FAmanca <fadlyamanca@gmail.com>
2025-02-04 14:27:51 +0700

    nambah edit-hapus buat spp

--- app/Http/Controllers/SppController.php
+++ app/Http/Controllers/SppController.php
@@ -8,7 +8,9 @@ use Illuminate\Http\Request;
 class SppController extends Controller
 {
     public function create() {
-        return view('spp.buatspp');
+        return view('spp.buatspp',[
+            'data' => null
+        ]);
     }
 
     public function store(Request $request) {
@@ -23,5 +25,29 @@ class SppController extends Controller
         $spp->nominal = $request->nominal;
         $spp->save();
         return back()->with('success', 'Data SPP Berhasil Ditambahkan');
+    }
+
+    public function edit(Request $request, Spp $spp) {
+        return view('spp.edit',[
+            'data' => $spp,
+        ]);
+    }
+
+    public function update(Request $request, Spp $spp) {
+        $request->validate([
+            'tahun' => 'required',
+            'semester' => 'required|in:ganjil,genap',
+            'nominal' => 'required|numeric',
+        ]);
+        $spp->tahun = $request->tahun;
+        $spp->semester = $request->semester;
+        $spp->nominal = $request->nominal;
+        $spp->save();
+        return back()->with('success', 'Data SPP Berhasil Diperbarui');
+    }
+
+    public function destroy(Spp $spp) {
+        $spp->delete();
+        return back()->with('Data Spp Berhasil Dihapus');
     }
 }
